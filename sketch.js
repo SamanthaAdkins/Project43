41076d6c21435163be01bdc3dcfd27a0392a6c66
SamanthaAdkins <75395786+SamanthaAdkins@users.noreply.github.com>
2021-03-09 13:57:59 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle; 
+
+function setup(){
+    createCanvas(400,400); 
+    angleMode(DEGREES);   
+}
+
+function draw(){
+    background(204,229,255);
+
+    translate(200,200)
+    rotate(-90)
+
+    hr = hour();
+    mn = minute();
+    sc = second();
+
+    scAngle = map(sc, 0, 60, 0, 360);
+    mnAngle = map(mn,0,60,0,360)
+    hrAngle = map(hr % 12,0,12,0,360)
+
+    //drawing sc hand
+    push();
+    rotate(scAngle);
+    stroke(153,51,255);
+    strokeWeight(7);
+    line(0,0,100,0);
+    pop()
+
+    //drawing mins hand
+    push();
+    rotate(mnAngle);
+    stroke(255,0,255);
+    strokeWeight(7);
+    line(0,0,75,0);
+    pop()
+
+    //drawing hr hand
+    push();
+    rotate(hrAngle);
+    stroke(255,204,229);
+    strokeWeight(7);
+    line(0,0,50,0);
+    pop()
+
+    stroke(255,153,204);
+    point(0,0)
+
+    //drawing the arcs
+    strokeWeight(10);
+    noFill();
+    //Seconds
+    stroke(153,51,255);
+    arc(0,0,300,300,0,scAngle);
+    //Minutes
+    stroke(255,0,255);
+    arc(0,0,280,280,0,mnAngle);
+    //Hour
+    stroke(255,204,229);
+    arc(0,0,260,260,0,hrAngle);
+}
